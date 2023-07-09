263fb87e70097b03b90900656ab0d1424a575e34
meghamalviya5 <megha.malviya444@gmail.com>
2023-07-09 21:56:40 +0530

    trip advisor changes-

--- src/pages/DestinationDetails/DestinationDetails.jsx
+++ src/pages/DestinationDetails/DestinationDetails.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const DestinationDetails = () => {
-  return <div>DestinationDetails</div>;
+  const { continentID, countryID, destinationID } = useParams();
+
+  const continent = data.continents.find(
+    (cont) => cont.id === Number(continentID)
+  );
+  const country = continent.countries.find(
+    (country) => country.id === Number(countryID)
+  );
+  const destination = country.destinations.find(
+    (des) => des.id === Number(destinationID)
+  );
+
+  return (
+    <div>
+      <h1>{destination.name}</h1>
+      <div>
+        <div>{destination.image}</div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default DestinationDetails;
